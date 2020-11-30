84f77d1cf045af352b26a2102dc3aa7b539db973
dejagaples <febrymahfuz@gmail.com>
2020-11-30 15:24:28 +0800

    fixguru

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var rootRef = firebase.database().ref().child("kuis_1");
+
+rootRef.on("child_added", snap => {
+    var nama = snap.child("nama").val();
+    var kelas = snap.child("kelas").val();
+    var nilai = snap.child("nilai").val();
+
+$("#table_body").append("<tr><td>"+ nama+"</td><td>"+kelas+"</td><td>"+nilai+"</td></tr>");
+});
