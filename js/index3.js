4b2856bb7baf30130a0127eeb889b1c60a3743c0
dejagaples <febrymahfuz@gmail.com>
2020-12-11 00:01:36 +0800

    insyaAllahfix

--- js/index3.js
+++ js/index3.js
@@ -3,6 +3,8 @@ var rootRef = firebase.database().ref().child("kuis_3");
 rootRef.on("child_added", snap => {
     var nama = snap.child("nama").val();
     var kelas = snap.child("kelas").val();
+    var tanggal = snap.child("tanggal").val();
+    var waktu = snap.child("waktu").val();
     var no1 = snap.child("no1").val();
     var no2 = snap.child("no2").val();
     var no3 = snap.child("no3").val();
@@ -10,5 +12,5 @@ rootRef.on("child_added", snap => {
     var no5 = snap.child("no5").val();
     var nilai = snap.child("nilai").val();
 
-    $("#table_body").append("<tr><td>"+ nama+"</td><td>"+kelas+"</td><td>"+no1+"</td><td>"+no2+"</td><td>"+no3+"</td><td>"+no4+"</td><td>"+no5+"</td><td>"+nilai+"</td></tr>");
+    $("#table_body").append("<tr><td>"+ nama+"</td><td>"+kelas+"</td><td>"+tanggal+"</td><td>"+waktu+"</td><td>"+no1+"</td><td>"+no2+"</td><td>"+no3+"</td><td>"+no4+"</td><td>"+no5+"</td><td>"+nilai+"</td></tr>");
 });
