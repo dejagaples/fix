cd139c4d6e9defd0f06b0ff78f75948d83ca80c8
dejagaples <febrymahfuz@gmail.com>
2021-03-04 18:27:43 +0800

    validasibos

--- js/nav.js
+++ js/nav.js
@@ -11,3 +11,17 @@ $("#menu > li > a").click(function(e){
     $(this).parent().children("ul").stop(true,true).slideDown("normal");
     }
 });
+
+$("#menu2 > li > a").click(function(e){
+    e.preventDefault();
+    if($(this).hasClass("buka"))
+    {
+    $(this).removeClass("buka");
+    $(this).parent().children("ul").stop(true,true).slideUp("normal");
+    } else {
+    $("#menu2 a.tutup.buka").removeClass("buka");
+    $(this).addClass("buka");
+    $(".sub2").filter(":visible");
+    $(this).parent().children("ul").stop(true,true).slideDown("normal");
+    }
+});
